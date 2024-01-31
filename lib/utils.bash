ecfd39063aff39fc997f9884bd313114756f6c7a
Ilya Bersenev <ilya.n.bersenev@gmail.com>
2024-01-31 15:45:24 +0200

    Update urls

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for xcresults
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/${version}/xcresults"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
